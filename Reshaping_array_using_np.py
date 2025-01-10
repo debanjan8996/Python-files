ebb63739f2939a81d5ff83c511405977a2a2c5ae
Debanjan mondal <150779149+debanjan8996@users.noreply.github.com>
2025-01-10 12:01:13 +0530

    Create Reshaping_array_using_np.py

---
+++ Reshaping_array_using_np.py
@@ -0,0 +1,5 @@
+import numpy as np
+number_array=np.array([4,5,2,3,5,6,8,5])
+reshaped_number_array=number_array.reshape(4,2)
+print ("\n Actual Array is : ",number_array)
+print ("\n The Reshaped Array is : ",reshaped_number_array)
