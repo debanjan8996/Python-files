f06aa57d43d283685dd02f4c7f6034db7f8a56e6
Debanjan mondal <150779149+debanjan8996@users.noreply.github.com>
2025-01-10 11:45:34 +0530

    Add files via upload

---
+++ append and extend.py
@@ -0,0 +1,17 @@
+list1 = ['a', 'b', 'c']
+list2 = ['d', 'e']
+
+# Append 'f' to list1.
+list1.append('f')
+
+# Print list1.
+print(list1)
+
+# Extend list1 with list2.
+list1.extend(list2)
+
+# Print list1.
+print(list1)
+
+# Print list2.
+print(list2)
